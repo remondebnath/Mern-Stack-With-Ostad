e36246681d08e2ad06be836cf65dbd1fbafc939d
Remon <remondebnath88@gmail.com>
2023-07-28 23:13:53 +0600

    commit

---
+++ Module01/livetest/main.js
@@ -0,0 +1,17 @@
+function getLastItem(arr){
+    return arr[arr.length - 1];
+
+}
+console.log(getLastItem([1,2,3]))
+console.log(getLastItem(["cat", "dog", "duck"]) )
+
+
+
+
+function getFirstValue(arr){
+
+
+}
+
+
+console.log("hi world")
