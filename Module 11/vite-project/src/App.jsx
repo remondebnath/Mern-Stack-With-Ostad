df213701f94b27057f3817363ac84470c1e4d43a
Remon <remondebnath88@gmail.com>
2023-12-16 00:19:44 +0600

    formData

--- Module 11/vite-project/src/App.jsx
+++ Module 11/vite-project/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import About from "./components/About";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
@@ -16,9 +16,14 @@ import Message from "./components/Message";
 
 const App =()=>{
 
+let [FormObj,SetFormObj]=useState({ fName:"", lname:"",city:"",gender:""})
+
+
+
+
   return(
-    <div>
-      <Header/>
+    
+    /* <Header/>
       <Hero/>
       <Service/>
       <Project/>
@@ -27,9 +32,33 @@ const App =()=>{
       <Person name="Remon"/>
       <Greetings/>
       <Temperature/>
-      <Message dayOfWeek={new Date().getDay()}/>
+      <Message dayOfWeek={new Date().getDay()}/> */
 
-    </div>
+
+      <div>
+
+        <form>
+
+          <input placeholder="First Name"/><br/>
+          <input placeholder="Last  Name"/><br/>
+          <select>
+            <option value="">Choose City</option>
+            <option value="Dhaka">Dhaka</option>
+            <option value="Noakhali">Noakhali</option>
+          </select><br/>
+          <input type="radio" value="male"/>Male<br/>
+          <input type="radio" value="male"/>Female<br/>
+          <button>Submit</button>
+
+
+
+
+
+
+        </form>
+
+
+      </div>
     
 
 
